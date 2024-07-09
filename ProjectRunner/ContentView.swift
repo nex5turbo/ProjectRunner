a8d23e47024b1ab3f825a89ac6dbfede7c02c4e5
Wonyoung Jang <nex5turbo@gmail.com>
2024-07-09 15:33:55 +0900

    Initial Commit

---
+++ ProjectRunner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectRunner
+//
+//  Created by 워뇨옹 on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectRunner/ProjectRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectRunnerApp.swift
+//  ProjectRunner
+//
+//  Created by 워뇨옹 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
