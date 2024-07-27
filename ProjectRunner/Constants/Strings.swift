2e204c24fe82836df12541f2c1336db4ee10bd60
Wonyoung Jang <nex5turbo@gmail.com>
2024-07-27 18:24:26 +0900

    premium button

---
+++ ProjectRunner/Constants/Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Strings.swift
+//  ProjectRunner
+//
+//  Created by 워뇨옹 on 7/27/24.
+//
+
+import Foundation
+
+extension String {
+    static let createMoreTasks: String = "Subscribe and manage more than 10 tasks per project!"
+    static let createMoreProjects: String = "Subscribe and manage more than 2 projects!"
+    static let createMoreDiaries: String = "Subscribe and save more than 10 diaries!"
+}
