2f9a4917cba8cd5623ff47342129b25d1a4c2676
Wonyoung Jang <nex5turbo@gmail.com>
2024-07-29 23:56:21 +0900

    settings view

--- ProjectRunner/Views/SettingsView/SettingsView.swift
+++ ProjectRunner/Views/SettingsView/SettingsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
     @Binding var appData: AppData
@@ -35,21 +36,78 @@ struct SettingsView: View {
                 }
             }
 #endif
-            
-            Button("Set Tutorial") {
-                do {
-                    try appData.loadTutorial()
-                } catch {
-                    print(error.localizedDescription)
+            Section("Tutorial") {
+                Button("Set Tutorial") {
+                    do {
+                        try appData.loadTutorial()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
                 }
             }
             
-            Section("Creators") {
-                Text("Designed by Anfaloumrani")
-                Text("Created by Wonyoung Jang")
+            Section("About") {
+                Button("Created by Wonyoung Jang") {
+                    let appURL = URL(string: "instagram://user?username=j_w_y_j")!
+                    let application = UIApplication.shared
+
+                    if application.canOpenURL(appURL) {
+                        application.open(appURL)
+                    } else {
+                        // if Instagram app is not installed, open URL inside Safari
+                        let webURL = URL(string: "https://instagram.com/j_w_y_j")!
+                        application.open(webURL)
+                    }
+                }
+                
+                Button("Designed by Anfaloumrani") {
+                    let appURL = URL(string: "instagram://user?username=anfaloumrani")!
+                    let application = UIApplication.shared
+
+                    if application.canOpenURL(appURL) {
+                        application.open(appURL)
+                    } else {
+                        // if Instagram app is not installed, open URL inside Safari
+                        let webURL = URL(string: "https://www.behance.net/gallery/194635105/Everglow-chocolate-visual-identity?fbclid=PAZXh0bgNhZW0CMTEAAaYdmxZJMDHYKRC0EQ3U1rZs3XIBmGKIKVypafrTwNpJTwc3nSsglb6JQ7A_aem_iKbywhKyeVWn4ubt27TsKA")!
+                        application.open(webURL)
+                    }
+                }
+
+                
+                Button("Privacy Policy") {
+                    guard let url = URL(string: "https://sites.google.com/view/project-runner-pp/") else {
+                        return
+                    }
+                    guard UIApplication.shared.canOpenURL(url) else {
+                        return
+                    }
+                    UIApplication.shared.open(url)
+                }
+                
+                Button("View App Permissions") {
+                    UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+                }
+                
+                Button("Rate") {
+                    guard let currentScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+                        return
+                    }
+                    SKStoreReviewController.requestReview(in: currentScene)
+                }
+            }
+            
+            Section("Version") {
+                Text(getVersion())
             }
         }
         .navigationTitle("Settings")
+    }
+    
+    func getVersion() -> String {
+        let dictionary = Bundle.main.infoDictionary
+        let version = dictionary?["CFBundleShortVersionString"] as? String
+        let build = dictionary?["CFBundleVersion"] as? String
+        return "\(version ?? "Unknown") (\(build ?? "Unknown"))"
     }
 }
 
